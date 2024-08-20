4126d826364f928d3bdba53eacc04d7df069c5dd
RyuKoBaihu <yamamoto.araujo.matheus@gmail.com>
2024-08-19 21:37:08 -0300

    setup

---
+++ management-sales/src/main/java/com/example/sales/management_sales/ManagementSalesApplication.java
@@ -0,0 +1,13 @@
+package com.example.sales.management_sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementSalesApplication.class, args);
+	}
+
+}
---
+++ management-sales/src/test/java/com/example/sales/management_sales/ManagementSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sales.management_sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
